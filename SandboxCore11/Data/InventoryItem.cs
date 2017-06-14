3dca40e6c3fdad6c18f0ff77b7daad25a03bfacf
ross.whitehead <ross.whitehead@civica.co.uk>
2017-06-14 09:15:34 +0100

    Add production publish profile

--- SandboxCore11/Data/InventoryItem.cs
+++ SandboxCore11/Data/InventoryItem.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,9 @@ namespace SandboxCore11.Data
     public class InventoryItem
     {
         public int Id { get; set; }
+
+        [StringLength(100, MinimumLength = 3)]
+        [Required]
         public string Name { get; set; }
     }
 }
