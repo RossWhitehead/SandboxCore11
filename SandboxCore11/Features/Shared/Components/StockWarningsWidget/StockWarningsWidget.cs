f980425d274ed454267806a2bd06c13ef72500b8
ross.whitehead <ross.whitehead@civica.co.uk>
2017-06-16 15:57:12 +0100

    Fix stylecop warning

--- SandboxCore11/Features/Shared/Components/StockWarningsWidget/StockWarningsWidget.cs
+++ SandboxCore11/Features/Shared/Components/StockWarningsWidget/StockWarningsWidget.cs
@@ -1,11 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace SandboxCore11.Features.Shared.Components
+﻿namespace SandboxCore11.Features.Shared.Components
 {
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Mvc;
+
     public class StockWarningsWidget : ViewComponent
     {
         public async Task<IViewComponentResult> InvokeAsync()
