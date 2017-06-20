1185dd15ba693fc834b61da6534ce9c66781c004
ross.whitehead <ross.whitehead@civica.co.uk>
2017-06-20 10:54:45 +0100

    Refactor home page into components

--- SandboxCore11/Features/Home/HomeController.cs
+++ SandboxCore11/Features/Home/HomeController.cs
@@ -1,32 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using SandboxCore11.Data;
-
-namespace SandboxCore11.Features.Home
+﻿namespace SandboxCore11.Features.Home
 {
+    using System;
+    using System.Collections.Generic;
+    using Microsoft.AspNetCore.Mvc;
+    using SandboxCore11.Data;
+
     public class HomeController : Controller
     {
-        public IActionResult Index([FromServices]ApplicationDbContext db)
+        public IActionResult Index()
         {
-            var orders = new List<PurchaseOrder>()
-            {
-                new PurchaseOrder() { Name = "Donec id elit non", LastUpdated = DateTime.Now.AddDays(-2), Description = "Maecenas sed diam eget risus varius blandit. Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit. Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit." },
-                new PurchaseOrder() { Name = "Maecenas sed diam eget risus", LastUpdated = DateTime.Now.AddDays(-3), Description = "Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit. Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit." },
-                new PurchaseOrder() { Name = "Maecenas sed diam eget risus", LastUpdated = DateTime.Now.AddDays(-8), Description = "Maecenas sed diam eget risus varius blandit. Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit." },
-                new PurchaseOrder() { Name = "Donec id elit non", LastUpdated = DateTime.Now.AddDays(-2), Description = "Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit. Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit." },
-                new PurchaseOrder() { Name = "Maecenas sed diam eget risus", LastUpdated = DateTime.Now.AddDays(-5), Description = "Maecenas sed diam eget risus varius blandit. Maecenas sed diam eget risus varius blandit. Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit." },
-                new PurchaseOrder() { Name = "Donec id elit non", LastUpdated = DateTime.Now.AddDays(-15), Description = "Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit." }
-            };
-
-            var vm = new IndexViewModel()
-            {
-                Orders = orders
-            };
-
-            return View(vm);
+           return View();
         }
 
         public IActionResult About()
--- SandboxCore11/wwwroot/js/Scripts/site.js
+++ SandboxCore11/wwwroot/js/Scripts/site.js
@@ -1,11 +1,13 @@
 var SandboxCore11;
 (function (SandboxCore11) {
-    class Message {
-        display(message) {
-            console.log('Message' + message);
+    var Message = (function () {
+        function Message() {
         }
-    }
+        Message.prototype.display = function (message) {
+            console.log('Message' + message);
+        };
+        return Message;
+    }());
     SandboxCore11.Message = Message;
-    console.log($('.table'));
 })(SandboxCore11 || (SandboxCore11 = {}));
 //# sourceMappingURL=site.js.map
